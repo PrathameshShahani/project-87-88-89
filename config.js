53a2f67392879b22af2b6e3db93aa656a8880c41
PrathameshShahani <73947902+PrathameshShahani@users.noreply.github.com>
2021-11-10 12:32:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAdSlN9X8U2gJDUclRhZbOchlz3dWmDlx8",
+    authDomain: "storytelling-app-a9631.firebaseapp.com",
+    projectId: "storytelling-app-a9631",
+    storageBucket: "storytelling-app-a9631.appspot.com",
+    messagingSenderId: "204314842777",
+    appId: "1:204314842777:web:f36ca5e94dafd5186e8a08"
+  };
